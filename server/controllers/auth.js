5a6fa2bd5aee5b56420e329d653ec8d34a8ef233
Matt Lovan <magrelo404@gmail.com>
2017-11-12 12:31:55 -0500

    update callback url

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -14,7 +14,7 @@ exports.getTwitterRequestToken = (request, response) => {
     fetch.post({
       url: 'https://api.twitter.com/oauth/request_token',
       oauth: {
-        oauth_callback: "http%3A%2F%2Flocalhost%3A3000%2Ftwitter-callback",
+        oauth_callback: "http%3A%2F%2Fstake.servesa.io%2Ftwitter-callback",
         consumer_key: twitterConsumerKey,
         consumer_secret: twitterSecret
       }
